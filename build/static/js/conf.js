297dce6a15dd2cf85640540e68c9c03f4e95b09a
kicha3007 <kicha3007@gmail.com>
2018-09-13 20:36:50 +0500

    devel

--- build/static/js/conf.js
+++ build/static/js/conf.js
@@ -1,45 +1,90 @@
 /* ------------------- calculation ----------------------- */
 
-var stepCounter = +1;
+$(function () {
 
-var btnPrev = $("[data-it-btn='prev']");
-var btnNext = $("[data-it-btn='next']");
-var btnSend = $("[data-it-btn='send']");
-var calcTitle = $("[data-calculation-title]");
+    // Слайдер синхронизированный
+
+    $('.slider-for').slick({
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        // // asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        // centerMode: true,
+        focusOnSelect: true
+    });
+
+})
+
+
+var stepCounter = 1;
+
+var btnNext = $("[data-it-btn='next']"),
+    btnPrev = $("[data-it-btn='prev']");
+
+
+// const conf = {
+//     character:  $("[data-it-character]"),
+//     facts:  $("[data-it-facts]"),
+//     model:  $("[data-it-model]"),
+//     prepayment:  $("[data-it-prepayment]"),
+//     print:  $("[data-it-print]"),
+//     options:  $("[data-it-options]"),
+//     mainContent:  $("[data-it-main-content]"),
+//     form:  $("[data-it-form]"),
+//     allSum:  $("[data-it-all-sum]"),
+// }
+
+
+// var btnSend = $("[data-it-btn='send']");
+// var calcTitle = $("[data-calculation-title]");
 
 
 function calculation() {
+    var dotLine = $("[data-it-dot-line='" + stepCounter + "']");
+    var confActiveParts = $("[data-it-conf-part='" + stepCounter + "']");
+    var prevConfParts = $("[data-it-conf-part='" + (stepCounter - +1) + "']");
 
-    $("[data-it-calculation-item]").hide();
-    $("[data-it-calculation-item='" + stepCounter + "']").show();
-    var dotLine = $("[data-dot-line='" + stepCounter + "']");
-    var calculationText = $("[data-calculation-text='" + stepCounter + "']");
     switch (stepCounter) {
         case 1:
-            btnPrev.removeClass("active");
-            dotLine.next().removeClass("active");
-            calculationText.addClass("active");
-            calculationText.next().removeClass("active");
-            calculationText.parent().next().find("[data-calculation-text]").removeClass("active");
+
+            confActiveParts.addClass("active");
+
+            // btnPrev.removeClass("active");
+            // dotLine.next().removeClass("active");
+            // calculationText.addClass("active");
+            // calculationText.next().removeClass("active");
+            // calculationText.parent().next().find("[data-calculation-text]").removeClass("active");
             break;
         case 2:
-            btnPrev.addClass("active");
-            btnSend.removeClass("active");
-            btnNext.addClass("active");
-            dotLine.addClass("active");
-            dotLine.next().removeClass("active");
-            calculationText.addClass("active");
-            calculationText.parent().next().find("[data-calculation-text]").removeClass("active");
-            calculationText.parent().prev().find("[data-calculation-text]").removeClass("active");
+            confActiveParts.addClass("active");
+            prevConfParts.removeClass("active");
+            // btnPrev.addClass("active");
+            // btnSend.removeClass("active");
+            // btnNext.addClass("active");
+            // dotLine.addClass("active");
+            // dotLine.next().removeClass("active");
+            // calculationText.addClass("active");
+            // calculationText.parent().next().find("[data-calculation-text]").removeClass("active");
+            // calculationText.parent().prev().find("[data-calculation-text]").removeClass("active");
             break;
         case 3:
-            btnNext.removeClass("active");
-            btnPrev.addClass("active");
-            btnSend.addClass("active");
-            dotLine.addClass("active");
-            calculationText.addClass("active");
-            calculationText.parent().prev().find("[data-calculation-text]").removeClass("active");
-            getCalculationValue();
+            confActiveParts.addClass("active");
+            prevConfParts.removeClass("active");
+
+            // btnNext.removeClass("active");
+            // btnPrev.addClass("active");
+            // btnSend.addClass("active");
+            // dotLine.addClass("active");
+            // calculationText.addClass("active");
+            // calculationText.parent().prev().find("[data-calculation-text]").removeClass("active");
+            // getCalculationValue();
             break;
         case 4:
     }
@@ -47,23 +92,23 @@ function calculation() {
 
 
 btnNext.on("click", function (e) {
-    $(".it-validate-color").removeClass("it-validate-color");
-    $(".it-validate-error").removeClass("active");
-
-    $("[data-it-calculation-item='" + stepCounter + "'] [required]").each(function () {
-        var calcValidate = $(this).val();
-
-        if (calcValidate == null || calcValidate == "") {
-            $(this).addClass("it-validate-color");
-            $(".it-validate-error").addClass("active");
-
-        }
-    });
-
-    if (!$(".it-validate-error").hasClass("active")) {
-        stepCounter++;
-        calculation();
-    }
+    // $(".it-validate-color").removeClass("it-validate-color");
+    // $(".it-validate-error").removeClass("active");
+    //
+    // $("[data-it-calculation-item='" + stepCounter + "'] [required]").each(function () {
+    //     var calcValidate = $(this).val();
+    //
+    //     if (calcValidate == null || calcValidate == "") {
+    //         $(this).addClass("it-validate-color");
+    //         $(".it-validate-error").addClass("active");
+    //
+    //     }
+    // });
+    //
+    // if (!$(".it-validate-error").hasClass("active")) {
+    stepCounter++;
+    calculation();
+    // }
 });
 
 btnPrev.on("click", function (e) {
